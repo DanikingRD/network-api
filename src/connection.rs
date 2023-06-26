67c82d789d07d9599d0ac091282151e701633c46
DanikingRD <danikingrd@gmail.com>
2023-06-25 22:08:56 -0400

    Implement peek fn

--- src/connection.rs
+++ src/connection.rs
@@ -1,22 +1,24 @@
 use std::net::{SocketAddr, Ipv4Addr};
 use std::io::Error;
 
+use serde::Serialize;
+use serde::de::DeserializeOwned;
 use tokio::net::UdpSocket;
-
 use crate::bind_udp_socket;
 
 pub struct Connection {
     sock: UdpSocket,
 }
 
-impl Connection{ 
+const PACKET_BUFFER_SIZE: usize = 1024;
+
+impl Connection { 
 
     pub async fn connect(addr: SocketAddr) -> Result<Self, Error> {
         let this = Self::bind_to_any()?;
         this.sock.connect(addr).await?;
         Ok(this)
     }
-  
 
     pub fn bind_to_any() -> Result<Self, Error> {
         Self::bind_to(SocketAddr::from((Ipv4Addr::UNSPECIFIED, 0)))
@@ -24,8 +26,42 @@ impl Connection{
 
     pub fn bind_to(addr: SocketAddr) -> Result<Self, Error> {
         Ok( Self {
-            sock:  bind_udp_socket(addr)?,
+            sock: bind_udp_socket(addr)?,
         })
+    }
+
+    pub async fn send<P: Serialize>(&self, packet: P)  {
+        let buffer = Self::serialize(&packet);
+        self.sock.send(&buffer).await.expect("Failed to send packet");
+    }
+
+    pub async fn recv<P: DeserializeOwned>(&self) -> Result<P, Error> {
+        let mut buffer = [0u8; PACKET_BUFFER_SIZE];
+        match self.sock.recv(&mut buffer).await {
+            Ok(len) => Self::deserialize::<P>(&buffer[0..len]),
+            Err(e) => Err(e),
+        }
+    }
+
+    pub async fn peek<P: DeserializeOwned>(&self) -> Result<(P, SocketAddr), Error> {
+        let mut buffer = [0u8; PACKET_BUFFER_SIZE];
+        match self.sock.peek_from(&mut buffer).await {
+            Ok((len, addr)) => Self::deserialize::<P>(&buffer[0..len]).map(|p| (p, addr)),
+            Err(e) => Err(e),
+        }
+    }
+
+    pub fn serialize<P: Serialize>(p: &P) -> Vec<u8> {
+        bincode::serialize(p).unwrap()
+    }
+
+    pub fn deserialize<P: DeserializeOwned>(bytes: &[u8]) -> Result<P, std::io::Error> {
+        return match bincode::deserialize::<P>(&bytes) {
+            Ok(t) => Ok(t),
+            Err(e) => {
+                Err(std::io::Error::new(std::io::ErrorKind::Other, e))
+            },
+        };
     }
 
     pub fn addr(&self) -> SocketAddr {
@@ -39,13 +75,40 @@ impl Connection{
 
 #[cfg(test)]
 mod tests {
+    use crate::messages::ClientMessage;
+
+    use super::*;
 
     #[tokio::test]
     async fn test_constructor() {
-        use super::*;
         let addr = SocketAddr::from(([127, 0, 0, 1], 0));
         let server = Connection::bind_to(addr).unwrap();
         let client = Connection::connect(server.addr()).await.unwrap();
         assert_eq!(server.addr(), client.peer_addr());
+    }
+
+    #[tokio::test]
+    async fn send_recv() {
+        let addr = SocketAddr::from(([127, 0, 0, 1], 0));
+        let server = Connection::bind_to(addr).unwrap();
+        let client = Connection::connect(server.addr()).await.unwrap();
+
+        client.send(ClientMessage::Connect).await;
+        let packet = server.recv::<ClientMessage>().await.unwrap();
+        assert!(matches!(packet, ClientMessage::Connect));
+    }
+
+    #[tokio::test]
+    async fn send_peek_recv() {
+        let addr = SocketAddr::from(([127, 0, 0, 1], 0));
+        let server = Connection::bind_to(addr).unwrap();
+        let client = Connection::connect(server.addr()).await.unwrap();
+
+        client.send(ClientMessage::Connect).await;
+        let (packet, _) = server.peek::<ClientMessage>().await.unwrap();
+        assert!(matches!(packet, ClientMessage::Connect));
+
+        let packet = server.recv::<ClientMessage>().await.unwrap();
+        assert!(matches!(packet, ClientMessage::Connect));
     }
 }
--- src/message_center.rs
+++ src/message_center.rs
@@ -1,23 +1,37 @@
 use std::net::{SocketAddr};
 
-
 use tokio::{net::UdpSocket, runtime::Runtime};
 use std::io::Error;
-use crate::bind_udp_socket;
+use crate::{bind_udp_socket, connection::Connection, messages::ClientMessage};
 
 pub struct MessageCenter {
-    socket: UdpSocket,
+    con: Connection,
 }
 
 impl MessageCenter {
 
-  
-
-    /// Receive incoming messages
-    pub fn incoming(&self, rt: &Runtime) -> impl ExactSizeIterator<Item = MessageHandler> {
-        let handle = tokio::spawn(async move { handle_incoming().await });
-        rt.block_on(handle).unwrap()
+    pub fn new(addr: SocketAddr) -> Self {
+        let con = Connection::bind_to(addr).unwrap();
+        Self {
+            con,
+        }
     }
+
+    pub async fn accept(&self) -> impl ExactSizeIterator<Item = Connection> {
+        let mut cons = Vec::new();
+        loop {
+            // TODO: break when no more connections
+            match self.con.peek::<ClientMessage>().await {
+                Ok((ClientMessage::Connect, addr)) => {
+                    let con = Connection::connect(addr).await.unwrap();
+                    cons.push(con);
+                },
+                Ok(_) => continue,
+                Err(e) => break,
+            }
+        }
+        cons.into_iter()
+    }   
 }
 
 async fn handle_incoming() -> impl ExactSizeIterator<Item = MessageHandler> {
@@ -26,9 +40,7 @@ async fn handle_incoming() -> impl ExactSizeIterator<Item = MessageHandler> {
     handlers.into_iter()
 }
 
-pub struct MessageHandler {
-    
-}
+pub struct MessageHandler {}
 
 impl MessageHandler {
     pub fn new() -> Self {
@@ -36,4 +48,24 @@ impl MessageHandler {
 
         Self {}
     }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::messages::ClientMessage;
+
+    use super::*;
+
+    #[tokio::test]
+    async fn test_constructor() {
+        let addr = SocketAddr::from(([127, 0, 0, 1], 0));
+        let server = MessageCenter::new(addr);
+        let client = Connection::connect(addr).await.unwrap();
+
+        for _ in 0..10 {
+            client.send(ClientMessage::Connect).await;
+        }
+
+        server.accept();
+    }   
 }
